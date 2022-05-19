f05b9d2130566bcfeff7b286017c2962c0feca46
zhuyoujun <zhuyoujun0513@163.com>
2022-05-19 14:50:57 +0800

    Added event system + basic test.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,4 +1,8 @@
 #include "Application.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/MouseEvent.h"
+#include "Hazel/Log.h"
 
 namespace Hazel {
 
@@ -13,6 +17,39 @@ namespace Hazel {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HZ_INFO(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			HZ_INFO(e);
+		}
+
+		
+		MouseButtonPressedEvent mouse(1);
+		if (mouse.IsInCategory(EventCategoryMouse))
+		{
+			HZ_TRACE(mouse);
+		}
+		if (mouse.IsInCategory(EventCategoryInput))
+		{
+			HZ_TRACE(mouse);
+		}
+
+		KeyReleasedEvent key(1);
+		if (key.IsInCategory(EventCategoryKeyboard))
+		{
+			HZ_TRACE(key);
+		}
+		if (key.IsInCategory(EventCategoryInput))
+		{
+			HZ_TRACE(key);
+		}
+		
+		
 		while (true);
 	}
 }
